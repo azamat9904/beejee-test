d495ec3959b9208e433909b189abe15d04307481
Azamat9904 <azamatnamialy@gmail.com>
2020-11-14 19:39:04 +0600

    chore(): Deleting dev file

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
     require_once "application/config/config.php";
     require_once "application/config/routes.php";
-    require_once "application/libs/Dev.php";
     require_once "application/libs/checkInput.php";
     require_once "application/libs/feedback.php";
     require_once "application/libs/checkAuth.php";
